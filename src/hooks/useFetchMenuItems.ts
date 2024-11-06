28d8c26f578be7b185aaa09c013f3a343d886f0d
Kevin Zhang <kevzhang2022@gmail.com>
2024-11-06 12:46:15 -0600

    Update custom API hook

--- src/hooks/useFetchMenuItems.ts
+++ src/hooks/useFetchMenuItems.ts
@@ -3,11 +3,14 @@ import { MenuItemApi } from "../apis/menu-item-api";
 import MenuItem from "../models/MenuItem";
 
 const useFetchMenuItems = (client: MenuItemApi) => {
+    const [currRightPane, setCurrRightPane] = useState<RightPane>(RightPane.MenuItems);
     const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         const fetchMenuItems = async () => {
+            setLoading(true);
             try {
                 const data = await client.getMenuItems();
                 setMenuItems(data);
@@ -16,13 +19,15 @@ const useFetchMenuItems = (client: MenuItemApi) => {
                     setError(e.message);
                     console.error(e.message);
                 }
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchMenuItems();
     }, [client]);
 
-    return { menuItems, error };
+    return { menuItems, loading, error };
 };
 
 export default useFetchMenuItems;
