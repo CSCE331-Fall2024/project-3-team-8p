226706b4a957c912b8c1babaea0261d68de7bcfa
Soham Nagawanshi <ndsoham@tamu.edu>
2024-11-07 12:15:59 -0600

    Added try catch schema

--- rest-api/src/test/java/org/project3/rest_api/RestAPIApplicationTests.java
+++ rest-api/src/test/java/org/project3/rest_api/RestAPIApplicationTests.java
@@ -3,12 +3,14 @@ package org.project3.rest_api;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestAPIApplicationTests {
@@ -22,16 +24,23 @@ class RestAPIApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	String baseUrl = " ";
+	private String baseUrl;
 
-	String jsonPrettier(String uglyJson) throws Exception{
-		ObjectMapper objectMapper = new ObjectMapper();
-		Object jsonObject = objectMapper.readValue(uglyJson, Object.class);
-		String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+	String jsonPrettier(String uglyJson) {
+		String prettyJson = "";
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			Object jsonObject = objectMapper.readValue(uglyJson, Object.class);
+			prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+		} catch (Exception e) {
+			System.out.println("An error has occured: ");
+			e.printStackTrace();
+		}
+
 		return prettyJson;
 	}
 
-	void printResult(String rawJson, String name) throws Exception {
+	void printResult(String rawJson, String name) {
 		System.out.println(name+": ");
 		System.out.println(
 				jsonPrettier(rawJson)
