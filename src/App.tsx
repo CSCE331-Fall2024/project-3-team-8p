99fa1b61a9dfc630bf0c3e690ce2c44ada67ceb3
Shreyan Satheesh <s0901527@students.katyisd.org>
2024-11-10 11:53:41 -0600

    Fix cartprodiver

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,36 @@ import ManagerView from "./views/ManagerView";
 import CustomerView from "./views/CustomerView";
 import Checkout from "./components/customer/Checkout";
 import './App.css';
-import { CartProvider } from './context/CartContext';  // Import the CartProvider
+import { CartProvider } from './context/CartContext';
 
 function App() {
     return (
         <div className="panda-express-app">
             <Container className="mt-3">
-                <CartProvider>  {/* Wrap the whole app or just the relevant sections */}
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={
-                                <div className="home">
-                                    <Link to="/">Home</Link>
-                                    <Link to="/manager">Manager</Link>
-                                    <Link to="/customer">Customer</Link>
-                                </div>
-                            } />
-                            <Route path="/manager" element={<ManagerView />} />
-                            <Route path="/customer" element={<CustomerView />} />
-                            <Route path="/checkout" element={<Checkout />} />
-                        </Routes>
-                    </BrowserRouter>
-                </CartProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={
+                            <div className="home">
+                                <Link to="/">Home</Link>
+                                <Link to="/manager">Manager</Link>
+                                <Link to="/customer">Customer</Link>
+                            </div>
+                        } />
+                        <Route path="/manager" element={<ManagerView />} />
+
+                        {/* Wrap CustomerView and Checkout in CartProvider */}
+                        <Route path="/customer" element={
+                            <CartProvider>
+                                <CustomerView />
+                            </CartProvider>
+                        } />
+                        <Route path="/checkout" element={
+                            <CartProvider>
+                                <Checkout />
+                            </CartProvider>
+                        } />
+                    </Routes>
+                </BrowserRouter>
             </Container>
         </div>
     );
