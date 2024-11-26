6d7e91e5b826c534285ae38eea558c06065c283d
Kevin Zhang <kevzhang2022@gmail.com>
2024-11-25 21:22:04 -0600

    Configure for production

--- src/apis/base-api.ts
+++ src/apis/base-api.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from 'axios';
 Sends GET, POST, PUT, and DELETE requests to server
 */
 export default class BaseApi {
-    private readonly _baseUrl: string = "http://localhost:8080/api";
+    private readonly _baseUrl: string = process.env.REACT_APP_BACKEND_PRODUCTION_API_URL as string;
     protected apiClient: AxiosInstance;
 
     constructor(endpoint: string) {
