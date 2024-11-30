adaa769ceef2bf9cd7c7b6b0f51ff7b07a2af3f5
Kevin Zhang <kevzhang2022@gmail.com>
2024-11-30 16:53:19 -0600

    Add MenuItem category

--- src/models/MenuItem.ts
+++ src/models/MenuItem.ts
@@ -2,12 +2,17 @@ import InventoryItem from "./InventoryItem";
 import CardItem from "./interfaces/CardItem";
 import MenuItemDict from "./dict-types/MenuItemDict";
 import InventoryItemDict from "./dict-types/InventoryItemDict";
+import MenuItemCategory from "./enums/MenuItemCategory";
+import NutritionInfoDict from "./dict-types/NutritionInfoDict";
 
 export default class MenuItem implements CardItem {
     private readonly _menuItemId: string;
     private readonly _price: number;
     private readonly _itemName: string;
+    private readonly _category: MenuItemCategory;
+    private readonly _isDiscounted: boolean;
     private readonly _inventoryItems: InventoryItem[];
+    public readonly _nutritionInfo: NutritionInfoDict;
 
     private _translatedItemName: string = "";
 
@@ -15,7 +20,10 @@ export default class MenuItem implements CardItem {
         const menuItem = new MenuItem(
             dict.menuItemId,
             dict.price,
-            dict.itemName
+            dict.itemName,
+            dict.category as MenuItemCategory,
+            dict.isDiscounted,
+            dict.nutritionInfo,
         );
         dict.inventoryItems
             .sort((a: InventoryItemDict, b: InventoryItemDict) => a.itemName.localeCompare(b.itemName))
@@ -25,11 +33,21 @@ export default class MenuItem implements CardItem {
         return menuItem;
     }
 
-    constructor(menuItemId: string, price: number, itemName: string) {
+    constructor(
+        menuItemId: string,
+        price: number,
+        itemName: string,
+        category: MenuItemCategory,
+        isDiscounted: boolean,
+        nutritionInfo: NutritionInfoDict,
+    ) {
         this._menuItemId = menuItemId;
         this._price = price;
         this._itemName = itemName;
+        this._category = category;
+        this._isDiscounted = isDiscounted;
         this._inventoryItems = [];
+        this._nutritionInfo = nutritionInfo;
     }
 
     get id(): string {
@@ -56,6 +74,14 @@ export default class MenuItem implements CardItem {
         this._translatedItemName = value;
     }
 
+    get category(): MenuItemCategory {
+        return this._category;
+    }
+
+    get isDiscounted(): boolean {
+        return this._isDiscounted;
+    }
+
     get inventoryItems(): InventoryItem[] {
         return this._inventoryItems;
     }
@@ -64,17 +90,24 @@ export default class MenuItem implements CardItem {
         this._inventoryItems.push(inventoryItem);
     }
 
+    get nutritionInfo(): NutritionInfoDict {
+        return this._nutritionInfo;
+    }
+
     toDict(): MenuItemDict {
         return {
             menuItemId: this._menuItemId,
             price: this._price,
             itemName: this._itemName,
-            inventoryItems: this._inventoryItems.map((item: InventoryItem) => ({
+            category: this._category.toString(),
+            isDiscounted: this._isDiscounted,
+            inventoryItems: this._inventoryItems.map((item: InventoryItem): InventoryItemDict => ({
                 inventoryItemId: item.inventoryItemId,
                 cost: item.cost,
                 availableStock: item.availableStock,
                 itemName: item.itemName,
-            }))
+            })),
+            nutritionInfo: this._nutritionInfo,
         };
     }
 }
