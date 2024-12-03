85d1afd926f93075e6b8ac3b6ea9238fe15bd15d
Kevin Zhang <kevzhang2022@gmail.com>
2024-12-03 09:58:48 -0600

    change high contrast button color

--- src/views/shared/RouteLayoutWrapper.tsx
+++ src/views/shared/RouteLayoutWrapper.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from "./Header";
-import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 
 function RouteLayoutWrapper() {
